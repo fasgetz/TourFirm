34fe2993d354b04db6590ca33643dfd82c508970
fasgetz <fasgetz@yandex.ru>
2021-09-12 15:52:11 +0300

    Добавлена страница оферты в отношении обработки персональных данных и ссылки на эту страницу с других страниц

---
+++ TourFirm/Controllers/OfertaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourFirm.Controllers
+{
+    public class OfertaController : Controller
+    {
+
+        /// <summary>
+        /// Оферта обработки персональных данных
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult PersonalPrivacyOferta()
+        {
+            return View();
+        }
+    }
+}
